02cf6af83865e584c312d8545d88fd066b3d204d
Atish03 <atishpersonal003@gmail.com>
2025-04-14 11:19:20 +0530

    [FEAT] added new moods and fortunes

--- fortune.py
+++ fortune.py
@@ -6,19 +6,21 @@ ADM_NO = os.getenv("ADM_NO", "21JE0191")
 FORTUNES = {
     "happy": f"✨ {NAME.split()[0]}, you will have a great day today! ✨",
     "sad": f"😊 Don't worry {NAME.split()[0]}, tomorrow will be better. 😊",
-    "neutral": "😐 Just another day, nothing special. 😐"
+    "neutral": "😐 Just another day, nothing special. 😐",
+    "bored": "😴 Try to find something interesting to do! 😴",
+    "excited": f"🎉 Something amazing is coming your way {NAME.split()[0]}! 🎉"
 }
 
 print(f"🔮 Welcome to {NAME}'s Fortune Teller! ({ADM_NO}) 🔮")
 
 while True:
     try:
-        print("How are you feeling today? (happy/sad/neutral): ", end="")
+        print("How are you feeling today? (happy/sad/neutral/bored/excited): ", end="")
         feeling = input().strip().lower()
         if feeling in FORTUNES:
             print(FORTUNES[feeling])
         else:
             print("Invalid input. Please enter 'happy', 'sad', or 'neutral'.")
     except KeyboardInterrupt:
-        print("\nGoodbye! Have a great day!")
+        print(f"\nGoodbye {NAME.split()[0]}! Have a great day!")
         break
